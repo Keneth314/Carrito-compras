3e3c4bac30f26ad828af6c730edd10d762305ae4
Keneth Lopez <jkeneyth29@hotmail.com>
2023-02-07 16:02:56 -0500

    V 1.1: Fusion del menu en desktop

---
+++ script.js
@@ -0,0 +1,21 @@
+const navbar_email = document.querySelector("li.navbar-email")
+const desplegable = document.querySelector("div.desktop-menu")
+
+navbar_email.addEventListener("click", visible)
+
+
+
+
+
+
+
+
+// -----------FUNCIONES-----------------
+function visible(){
+    desplegable.classList.toggle("invisible")
+}
+
+
+
+
+
