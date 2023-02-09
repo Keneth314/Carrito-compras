6581973956ddef3ad05d53db65b1959d0fc3b9f9
Keneth Lopez <jkeneyth29@hotmail.com>
2023-02-09 00:38:48 -0500

    V 1.4: Lista de productos mobile y desktop

--- script.js
+++ script.js
@@ -63,3 +63,84 @@ function toggleVisibleProductDetail() {
     
 }
 
+const uwu = "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg"
+const productList = [];
+
+productList.push ({
+    name:'Bike',
+    price: 12700,
+    image: 'https://fastly.picsum.photos/id/63/300/200.jpg?hmac=fIasgHoSbyiqE83FdjmxzRR2vjfUKDNktGG8MNuO_4Q'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://fastly.picsum.photos/id/1041/300/200.jpg?hmac=6ezBGCQFpwkQX8Q2X5IwixcAgYYpqAT-d_4rMaG-bu4'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://fastly.picsum.photos/id/650/300/200.jpg?hmac=nuRnzrByBYKU6fsTl1xwz3MIpNrNjb0PjLJA5ypiOqU'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://fastly.picsum.photos/id/1081/300/200.jpg?hmac=fbKeKxgZkfNvHihq19otFEq3XIIskrkKx0agOvAsFlI'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://fastly.picsum.photos/id/735/300/200.jpg?hmac=POPDKxfv7tiKB212bnTjx60t4vzSUAWxwjHeIimVHWg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://fastly.picsum.photos/id/821/300/200.jpg?hmac=6IKXckQtVXfIh7pRXj7cxezrxH-zM2WAkCAcn2EGZao'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://fastly.picsum.photos/id/430/300/200.jpg?hmac=MFV20dJJadnnbIqXebEtuv2S-6-zEEMQXzkP7UK3HKU'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://fastly.picsum.photos/id/253/300/200.jpg?hmac=75Xcyfr-AhlvQYL85fVhv4SQ6xUrW1TiRnOzpAnlc1k'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://fastly.picsum.photos/id/59/300/200.jpg?hmac=NDge9tf7z_Kc8h3nccPRWXtXqV7kAjM93CMqRNN-3wU'
+}); 
+
+for(product of productList){
+    const cards_container = document.querySelector(".cards-container")
+
+    const product_card = document.createElement("div")
+    product_card.classList.add("product-card")
+    
+    const productImg = document.createElement("img")
+    productImg.setAttribute("src", product.image)
+    
+    const product_info = document.createElement("div")
+    product_info.classList.add("product-info")
+    
+    const product_div = document.createElement("div")
+    
+    const productPrice = document.createElement("p")
+    productPrice.innerText = "$ " + product.price
+    const productName = document.createElement("p")
+    productName.innerText = product.name
+    
+    const productFigure = document.createElement("figure")
+    
+    const cartImg = document.createElement("img")
+    cartImg.setAttribute("src", "./icons/bt_add_to_cart.svg")
+    
+    cards_container.appendChild(product_card);
+    product_card.append(productImg, product_info);
+    product_info.append(product_div, productFigure);
+    product_div.append(productPrice, productName);
+    productFigure.append(cartImg); 
+    
+}
+
